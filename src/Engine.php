346d56c223bf91036c5bc49453e16ae6a97a2d91
Ilya Zherebtsov <izherebcov@gmail.com>
2021-11-04 14:24:47 +0400

    add engine

---
+++ src/Engine.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Brain\Games;
+
+use function BrainGames\Cli\run;
+use function cli\line;
+use function cli\prompt;
+
+function engineGame($game, $conditionGame)
+{
+    $name = run();
+
+    line($conditionGame);
+
+    for ($i = 0; $i < 3; $i++) {
+        $arrayGames = $game();
+
+        line("Question: $arrayGames[0]");
+
+        $answerUser = prompt("Your answer");
+
+        if ($answerUser == $arrayGames[1]) {
+            line("Correct!");
+        } else {
+            line("'$answerUser' is wrong answer ;(. Correct answer was '$arrayGames[1]'.");
+            line("Let's try again, $name!");
+            exit;
+        }
+    }
+    line("Congratulations, $name!");
+}
